81ec8a2d8201ec21a285a189804da9e731da2a3c
fcoverdugoa <fcoverdugoa@underpost.net>
2021-04-18 08:58:51 -0400

    Add json type

---
+++ test/json.scm
@@ -0,0 +1,74 @@
+#lang scheme
+
+
+(require json)
+
+
+(require "./colorize.scm")
+
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+
+; https://docs.racket-lang.org/reference/data.html
+
+(pg_s "\n JSON TEST \n")
+
+(py_s "\n symbol | string |  number | boolean | list \n")
+
+(define json_content_a (call-with-input-file "./test.json" read-json))
+
+(for ( ( (key val) (in-hash json_content_a) ) )
+
+  ; (printf "~a = ~a~%" key val)
+
+  (begin
+
+    (py_s "new test ->")
+    (display key)
+    (display "\n")
+    (display (symbol? key))
+    (display (string? key))
+    (display (number? key))
+    (display (boolean? key))
+    (display (list? key))
+    (display "\n\n")
+    (display val)
+    (display "\n")
+    (display (symbol? val))
+    (display (string? val))
+    (display (number? val))
+    (display (boolean? val))
+    (display (list? val))
+    (display "\n\n")
+
+    void)
+
+)
+
+
+
+
+
+
+
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
